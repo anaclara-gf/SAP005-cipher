13479069936e34948a3ec8ebae99e2aad48d5322
Ana Clara <anaclaragf@outlook.com>
2020-10-28 13:46:09 -0300

    Criação dos métodos encode e decode dentro do objeto Cipher, criação dos eventos DOM no index.js e criação dos inputs no html

--- cipher.js
+++ cipher.js
@@ -1,5 +1,66 @@
 const cipher = {
-  // ...
+    encode(offset, message){
+        let encodeLetters;
+        let encodeMessage = "";
+        
+        if(message.length === 0){
+            alert("Escreva sua mensagem!")
+        };
+        
+        for(let i=0;i<message.length;i++){
+            let encodeASC = message.charCodeAt(i);
+        
+            if(encodeASC<48){
+                encodeLetters=encodeASC;
+            }else if(encodeASC>=48 && encodeASC<=57){
+                encodeLetters=((encodeASC-48)+offset)%26+48;
+            }else if(encodeASC>57 && encodeASC<65){
+                encodeLetters=encodeASC;
+            }else if(encodeASC>90 && encodeASC<97){
+                encodeLetters=encodeASC;
+            }else if(encodeASC>122){
+                encodeLetters=encodeASC;
+            }else if(encodeASC>=65 && encodeASC<=90){
+                encodeLetters=((encodeASC-65)+offset)%26+65;
+            }else if(encodeASC>=97 && encodeASC<=122){
+                encodeLetters=((encodeASC-97)+offset)%26+97;
+            }
+            
+        encodeMessage += String.fromCharCode(encodeLetters);
+        }
+        return encodeMessage;
+    },
+    
+    decode(offset, message){
+        let decodeLetters;
+        let decodeMessage = "";
+    
+        if(message.length === 0){
+            alert("Escreva sua mensagem!")
+        };
+        
+    
+        for(let i=0;i<message.length;i++){
+            let decodeASC = message.charCodeAt(i);
+    
+            if(decodeASC<65){
+                decodeLetters=decodeASC;
+            }else if(decodeASC>=48 && decodeASC<=57){
+                encodeLetters=((encodeASC-48)+(26-offset))%26+48;
+            }else if(decodeASC>90 && decodeASC<97){
+                decodeLetters=decodeASC;
+            }else if(decodeASC>122){
+                decodeLetters=decodeASC;
+            }else if(decodeASC>=65 && decodeASC<=90){
+                decodeLetters=((decodeASC-65)+(26-offset))%26+65;
+            }else if(decodeASC>=97 && decodeASC<=122){
+                decodeLetters=((decodeASC-97)+(26-offset))%26+97;
+            }
+    
+            decodeMessage += String.fromCharCode(decodeLetters)
+        }
+        return decodeMessage
+        }
 };
 
 export default cipher;
--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let buttonEncode = document.querySelector('#enviar-encode');
+
+function encode(event){
+    let offset = document.getElementById('offset-encode');
+    let message = document.getElementById('mensagem-encode');
+    let respostaHTML = document.querySelector('#resposta-encode');
+    let resposta = cipher.encode(parseInt(offset.value),message.value);
+    respostaHTML.textContent = resposta;
+
+    event.preventDefault()
+}
+
+buttonEncode.addEventListener("click",encode);
+
+let buttonDecode = document.querySelector('#enviar-decode');
+
+function decode(event){
+    let offset = document.getElementById('offset-decode');
+    let message = document.getElementById('mensagem-decode');
+    let respostaHTML = document.querySelector('#resposta-decode');
+    let resposta = cipher.decode(parseInt(offset.value),message.value);
+    respostaHTML.textContent = resposta;
+
+    event.preventDefault()
+}
+
+buttonDecode.addEventListener("click",decode);
