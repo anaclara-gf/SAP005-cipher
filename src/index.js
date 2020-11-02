99c69c25cb00449c407e9175dfb89d1a153ece68
Ana Clara <anaclaragf@outlook.com>
2020-11-02 15:35:43 -0300

    Cipher funcionando e passando nos testes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 import cipher from './cipher.js';
 
-let buttonEncode = document.querySelector('#enviar-encode');
+let buttonEncode = document.querySelector('#send-encode');
 
 function encode(event){
     let offset = document.getElementById('offset-encode');
-    let message = document.getElementById('mensagem-encode');
-    let respostaHTML = document.querySelector('#resposta-encode');
-    let resposta = cipher.encode(parseInt(offset.value),message.value);
-    respostaHTML.textContent = resposta;
+    let message = document.getElementById('message-encode');
+    let answerHTML = document.querySelector('#answer-encode');
+    try{
+        let answer = cipher.encode(parseInt(offset.value),message.value);
+        answerHTML.textContent = `Mensagem criptografada:\n${answer}`;
+    }catch(error){
+        alert(error);
+    }
 
     event.preventDefault()
 }
 
 buttonEncode.addEventListener("click",encode);
 
-let buttonDecode = document.querySelector('#enviar-decode');
+let buttonDecode = document.querySelector('#send-decode');
 
 function decode(event){
     let offset = document.getElementById('offset-decode');
-    let message = document.getElementById('mensagem-decode');
-    let respostaHTML = document.querySelector('#resposta-decode');
-    let resposta = cipher.decode(parseInt(offset.value),message.value);
-    respostaHTML.textContent = resposta;
+    let message = document.getElementById('message-decode');
+    let answerHTML = document.querySelector('#answer-decode');
+    try{
+        let answer = cipher.decode(parseInt(offset.value),message.value);
+        answerHTML.textContent = "Mensagem descriptografada: " + answer;
+    }catch(error){
+        alert(error);
+    }
 
     event.preventDefault()
 }
